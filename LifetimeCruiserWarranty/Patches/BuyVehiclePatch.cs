4bff24fad1e31a0c9bd1c2336fdeac1332f96b40
33mamaster <33mamaster@gmail.com>
2024-07-14 23:24:19 +1200

    First Version

---
+++ LifetimeCruiserWarranty/Patches/BuyVehiclePatch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LifetimeCruiserWarranty;
+using HarmonyLib;
+
+namespace LifetimeCruiserWarranty.Patches
+{
+	[HarmonyPatch(typeof(Terminal))]
+    internal class BuyVehiclePatch
+    {
+        [HarmonyPatch("BuyVehicleServerRpc")]
+        [HarmonyPostfix]
+        private static void BuyVehicleServerRpcPatch(ref bool ___hasWarrantyTicket)
+        {
+            ___hasWarrantyTicket = true;
+        }
+
+        [HarmonyPatch("BuyVehicleClientRpc")]
+        [HarmonyPostfix]
+        private static void BuyVehicleClientRpcPatch(ref bool ___hasWarrantyTicket)
+        {
+            ___hasWarrantyTicket = true;
+        }
+    }
+}
---
+++ LifetimeCruiserWarranty/Plugin.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace LifetimeCruiserWarranty
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private const string modGUID = "33mamaster.LifetimeCruiserWarranty";
+        private const string modName = "Lifetime Cruiser Warranty";
+        private const string modVersion = "1.0.0";
+
+        public static ConfigFile config;
+        internal static new ManualLogSource Logger;
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static Plugin Instance;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            Logger = base.Logger;
+            Logger.LogInfo($"Mod {modName} is loaded!");
+
+            harmony.PatchAll();
+        }
+    }
+}
