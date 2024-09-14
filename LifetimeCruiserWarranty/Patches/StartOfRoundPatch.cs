f68133a03ee2e24b4e4d8f2d8ee9b0e896eb5aad
33mamaster <33mamaster@gmail.com>
2024-09-14 17:30:29 +1200

    Stopped using pointer which can break during official updates
    As I no longer am attempting to inject code directly mid UI schedule, I don't need to interface with function directly.
    
    Instead run after method with regular harmony postpatch

---
+++ LifetimeCruiserWarranty/Patches/StartOfRoundPatch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+using UnityEngine;
+
+namespace LifetimeCruiserWarranty.Patches
+{
+    [HarmonyPatch(typeof(StartOfRound))]
+    internal class StartOfRoundPatch
+    {
+        [HarmonyPatch("PassTimeToNextDay")]
+        [HarmonyPostfix]
+        private static void PassTimeToNextDayPatch()
+        {
+            Plugin.Logger.LogInfo("Display Penalty Function");
+            if (Plugin.isVehicleLeftBehind)
+            {
+                HUDManager.Instance.StartCoroutine(DisplayAlert());
+            }
+        }
+
+        private static IEnumerator DisplayAlert()
+        {
+
+            yield return (object)new WaitForSeconds((float)4f);
+
+            // Display Punishment
+            HUDManager.Instance.DisplayTip("Cruiser Lost", $"The Company Cruiser was lost.\nFINES DUE: ${Plugin.due}", true, false, "LC_Warning1");
+
+            yield return (object)new WaitForSeconds((float)3f);
+        }
+    }
+}
